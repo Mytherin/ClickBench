6cde6e8a927e21bc9714602e3dcb7a4c3d566f4d
Mark Raasveldt <mark.raasveldt@gmail.com>
2022-11-12 12:38:07 +0100

    Update DuckDB scripts: remove primary key index, remove lowering of memory limit and threads, enable parallel CSV loading & disable insertion order preservation on load

--- duckdb/load.py
+++ duckdb/load.py
@@ -5,14 +5,13 @@ import timeit
 import psutil
 
 con = duckdb.connect(database="my-db.duckdb", read_only=False)
-# See https://github.com/duckdb/duckdb/issues/3969
-con.execute("PRAGMA memory_limit='{}b'".format(psutil.virtual_memory().total / 4))
-con.execute("PRAGMA threads={}".format(psutil.cpu_count(logical=False)))
 
 print("Will load the data")
 
 start = timeit.default_timer()
 con.execute(open("create.sql").read())
-con.execute("INSERT INTO hits SELECT * FROM read_csv_auto('hits.csv')")
+con.execute("SET experimental_parallel_csv=true")
+con.execute("SET preserve_insertion_order=false")
+con.execute("COPY hits FROM 'hits.csv'")
 end = timeit.default_timer()
 print(end - start)
--- duckdb/query.py
+++ duckdb/query.py
@@ -9,9 +9,6 @@ query = sys.stdin.read()
 print(query)
 
 con = duckdb.connect(database="my-db.duckdb", read_only=False)
-# See https://github.com/duckdb/duckdb/issues/3969
-con.execute("PRAGMA memory_limit='{}b'".format(psutil.virtual_memory().total / 4))
-con.execute("PRAGMA threads={}".format(psutil.cpu_count(logical=False)))
 
 for try_num in range(3):
     start = timeit.default_timer()
