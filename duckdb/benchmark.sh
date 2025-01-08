c3103256e33756f6169b3dd7f657e6f589e41779
Mark Raasveldt <mark.raasveldt@gmail.com>
2025-01-08 09:51:22 +0100

    Convert DuckDB script to use CLI instead of Python

--- duckdb/benchmark.sh
+++ duckdb/benchmark.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
 # Install
-
-sudo apt-get update
-sudo apt-get install -y python3-pip
-pip install --break-system-packages duckdb==1.1.3 psutil
+wget https://github.com/Mytherin/duckdb/tree/archnative
+cd duckdb
+NATIVE_ARCH=1 LTO=full make
+alias duckdb = `pwd`/build/release/duckdb
+cd ..
 
 # Load the data
-
 wget --no-verbose --continue 'https://datasets.clickhouse.com/hits_compatible/hits.csv.gz'
 gzip -d hits.csv.gz
+
+time duckdb hits.db -f create.sql -c "COPY hits FROM 'hits.csv'"
 
 ./load.py
 
@@ -17,7 +19,10 @@ gzip -d hits.csv.gz
 
 ./run.sh 2>&1 | tee log.txt
 
-wc -c my-db.duckdb
+wc -c hits.db
 
-cat log.txt | grep -P '^\d|Killed|Segmentation' | sed -r -e 's/^.*(Killed|Segmentation).*$/null\nnull\nnull/' |
-    awk '{ if (i % 3 == 0) { printf "[" }; printf $1; if (i % 3 != 2) { printf "," } else { print "]," }; ++i; }'
+cat log.txt |
+  grep -P '^real|^Error' |
+  sed -r -e 's/^Error.*$/null/; s/^real\s*([0-9.]+)m([0-9.]+)s$/\1 \2/' |
+  awk '{ if ($2) { print $1 * 60 + $2 } else { print $1 } }' |
+  awk '{ if ($1 == "null") { skip = 1 } else { if (i % 3 == 0) { printf "[" }; printf skip ? "null" : $1; if (i % 3 != 2) { printf "," } else { print "]," }; ++i; skip = 0; } }'
